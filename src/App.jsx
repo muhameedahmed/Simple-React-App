36d3185ad42b09d13fd29147e05550ee8cce9af2
Muhammed ahmed <muhameedahmed4472@gmail.com>
2025-04-10 20:17:03 +0200

    edit router

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './modules/Navbar'
-import React from 'react'
-import Fpage from './modules/Fpage'
-import Portfolio from './modules/Portfolio'
-import About from './modules/About'
-import Contactme from './modules/Contactme'
-import Lastsec from './modules/lastsec'
-import Layout from './modules/layout'
-import Errorpage from './modules/Errorpage'
-import { createBrowserRouter, RouterProvider , NavLink } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Layout from './modules/layout';
+import Fpage from './modules/Fpage';
+import Contactme from './modules/Contactme';
+import About from './modules/About';
+import Portfolio from './modules/Portfolio';
+import Errorpage from './modules/Errorpage';
 
 function App() {
-
- let Brouter= createBrowserRouter([
-    {path:'' ,element:<Layout/> ,children:[
-      {path:'Simple-React-App',element:<Fpage/>},
-      {path:'contact',element:<Contactme/>},
-      {path:'About',element:<About/>},
-      {path:'Portfolio',element:<Portfolio/>},
-      {path:'*',element:<Errorpage/>}
-    ]}
-  ])
   return (
-    <>
-    <RouterProvider router={Brouter}></RouterProvider>
-    </>
-  )
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Fpage />} />
+          <Route path="contact" element={<Contactme />} />
+          <Route path="about" element={<About />} />
+          <Route path="portfolio" element={<Portfolio />} />
+          <Route path="*" element={<Errorpage />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
 }
 
-export default App
+export default App;
--- src/modules/Errorpage.jsx
+++ src/modules/Errorpage.jsx
@@ -9,7 +9,7 @@ export default function Errorpage() {
           <p className="text-lg text-gray-700 mt-2">We couldn't find the page you're looking for.</p>
           <button 
             className="mt-4 px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-            onClick={() => navigate("/")}
+            onClick={() => navigate("/Simple-React-App")}
           >
             Go Home
           </button>
